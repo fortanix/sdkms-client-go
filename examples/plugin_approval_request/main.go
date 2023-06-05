9ae94cbeccb0c41bc5195b0996ba892dc3897d3f
Mohsen Zohrevandi <mohsen.zohrevandi@fortanix.com>
2023-06-05 10:54:41 -0700

    Fix example formatting

--- examples/plugin_approval_request/main.go
+++ examples/plugin_approval_request/main.go
@@ -3,14 +3,14 @@
 // This program demonstrates how to request approval to invoke an SDKMS plugin.
 // The Lua code for the plugin is listed below:
 //
-// function check(input)
-//   key = assert(Sobject { name = "very important key" })
-//   require_approval_for(key)
-// end
+//	function check(input)
+//	   key = assert(Sobject { name = "very important key" })
+//	   require_approval_for(key)
+//	end
 //
-// function run(input)
-//   return key:sign(input)
-// end
+//	function run(input)
+//	   return key:sign(input)
+//	end
 package main
 
 import (
